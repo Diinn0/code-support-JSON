b19d10ab074953cdc4c9a64a9de828fb5ccbd5d9
Diinn0craft <dinocraftdidi@gmail.com>
2020-03-21 11:35:51 +0100

    Update processCouleurs.php

--- processCouleurs.php
+++ processCouleurs.php
@@ -2,20 +2,22 @@
 
 if (!empty($_POST['couleur']) && isset($_POST['couleur'])) {
 
-    $responses = explode(" ", $_POST['couleur']);
-    $responses = array_unique($responses);
-    sort($responses);
+    $responses = explode(" ", $_POST['couleur']); // On converti la réponse en tableau
+    $responses = array_unique($responses); // On supprime les doublons du tableau
+    sort($responses); // On trie le tableau par ordre alphabétique
 
-    $jsonFile = file_get_contents('couleur.json');
-    $couleurs = json_decode($jsonFile, true);
+    $jsonFile = file_get_contents('couleur.json'); // On récupère le contenu du fichier couleur.json
+    $couleurs = json_decode($jsonFile, true); // On converti le json obtenu en tableau
 
-    $couleurs['couleurs']['list'] = array_merge($couleurs['couleurs']['list'], $responses);
-    $json = json_encode($couleurs, JSON_PRETTY_PRINT);
+    $couleurs['couleurs']['list'] = array_merge($couleurs['couleurs']['list'], $responses); // On fusionne le tableau de réponse avec le tableau existant
+    $json = json_encode($couleurs, JSON_PRETTY_PRINT); // On encode le tableau fusionné en json en conservant le formattage
 
-    unlink('couleur.json');
-    $file = fopen('couleur.json', 'x');
-    fwrite($file, $json);
-    fclose($file);
+    unlink('couleur.json'); // On supprime l'ancien couleur.json
+    // On supprime l'ancien fichier car modifier un fichier à un
+    // endroit précis demande de savoir sur quel octet précis commencer à ajouter les données.
+    $file = fopen('couleur.json', 'x'); // On crée un nouveau couleur.json
+    fwrite($file, $json); // On écrit le JSON dans le fichier
+    fclose($file); // On ferme le fichier
 
-    header('Location: index.php');
+    header('Location: index.php'); //On redirige sur la page d'accueil
 }
